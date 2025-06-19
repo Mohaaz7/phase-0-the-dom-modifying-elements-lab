df54f9705d4b59693fecc9ae7caefb01ebbef062
mohaaz12 <mohaaz3004@gmail.com>
2025-06-19 18:31:01 +0300

    dom modifying

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Write your code here!
+// Step 1: Remove the <main id="main"> element
+const main = document.getElementById("main");
+main.remove();
+
+// Step 2: Create a new <h1> element and assign it to a variable
+const newHeader = document.createElement("h1");
+
+// Step 3: Set the id of the newHeader to "victory"
+newHeader.id = "victory";
+
+// Step 4: Set the text content of newHeader
+newHeader.textContent = "Mohamed Amin is the champion";
+
+// Step 5: Append the newHeader to the body
+document.body.append(newHeader);
